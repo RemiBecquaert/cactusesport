97e1bfc70f05d3151101f0f531bec85c793afd53
Rémi <remi.becquaert35@gmail.com>
2022-07-05 16:58:23 +0000

    add team and contact pages

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -13,4 +13,28 @@ class BaseController extends AbstractController
     {
         return $this->render('base/index.html.twig', []);
     }
+
+    #[Route('/rainbow-six', name: 'rainbow-six')]
+    public function rainbowSix(): Response
+    {
+        return $this->render('base/rainbow-six.html.twig', []);
+    }
+
+    #[Route('/rocket-league', name: 'rocket-league')]
+    public function rocketLeague(): Response
+    {
+        return $this->render('base/rocket-league.html.twig', []);
+    }
+
+    #[Route('/master-duel', name: 'master-duel')]
+    public function masterDuel(): Response
+    {
+        return $this->render('base/master-duel.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('base/contact.html.twig', []);
+    }
 }
