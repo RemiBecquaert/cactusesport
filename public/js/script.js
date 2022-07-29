7625ead952d284972c3f62b9157793c6b8c5412b
Rémi <remi.becquaert35@gmail.com>
2022-07-29 00:26:19 +0000

    add misc

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+/*const box = document.getElementById('box');
+
+box.addEventListener('mouseover', function handleMouseOver() {
+  box.style.color = 'green';
+});
+
+box.addEventListener('click', function handleMouseOver(){
+    box.style.color = 'green';
+});
+
+box.addEventListener('mouseout', function handleMouseOut() {
+  box.style.color = 'white';
+});
+*/
