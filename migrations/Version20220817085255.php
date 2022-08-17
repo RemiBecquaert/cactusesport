c3ce086f1a8d041d572149a722d617a456d3941c
Rémi <remi.becquaert35@gmail.com>
2022-08-17 13:16:42 +0000

    change colors, add recruitment form, controller and entity

---
+++ migrations/Version20220817085255.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220817085255 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE candidature (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(100) NOT NULL, nom VARCHAR(100) NOT NULL, prenom VARCHAR(100) NOT NULL, telephone INT NOT NULL, reference INT NOT NULL, texte LONGTEXT NOT NULL, date_envoi DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE candidature');
+    }
+}
--- src/Controller/JoinController.php
+++ src/Controller/JoinController.php
@@ -6,11 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Form\JoinType;
+use App\Entity\Candidature;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+
 class JoinController extends AbstractController
 {
     #[Route('/join', name: 'join')]
-    public function join(): Response
+    public function join(Request $request, ManagerRegistry $doctrine): Response
     {
-        return $this->render('join/join.html.twig', []);
+        $candidature = new Candidature();
+        $form = $this->createForm(JoinType::class, $candidature);
+
+        if ($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em = $doctrine->getManager();
+                $candidature->setDateEnvoi(new \Datetime());
+                $em->persist($candidature);
+                $em->flush();
+
+                $this->addFlash('notice','Candidature envoyée !');
+                return $this->redirectToRoute('join');
+            }
+        }
+
+        return $this->render('join/join.html.twig', ['form' => $form->createView()]);
     }
 }
